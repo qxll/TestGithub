15f8a594b3a201e895e34772576149efa45e072d
qxll <2925735954@qq.com>
2018-10-13 08:55:18 +0800

    test message

---
+++ app/src/main/java/com/example/administrator/testgithub/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.testgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
